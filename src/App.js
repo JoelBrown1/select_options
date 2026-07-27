d12be574ae51847d3657670ed6012f07ef7165b2
Joel Brown <joel.anthony.brown1@gmail.com>
2026-07-26 21:01:55 -0400

    Wire App shell: Header, Sidebar, MainContent, and routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Sidebar from './components/Sidebar/Sidebar';
+import MainContent from './components/MainContent/MainContent';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="app-shell">
+        <Header />
+        <div className="app-shell__body">
+          <Sidebar />
+          <MainContent />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
